576d5c764973ab36529176e6ce14553a23ac50ee
Gabriel Ribeiro de Freitas <116754893+SrHut27@users.noreply.github.com>
2024-05-10 01:18:01 +0000

    planning database

--- server/extras/databaseInfo.sql
+++ server/extras/databaseInfo.sql
@@ -6,4 +6,12 @@ CREATE TABLE users (
     username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
-)
+);
+
+CREATE TABLE posts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user INT,
+    tittle VARCHAR(255) NOT NULL,
+    content text,
+    FOREIGN KEY (user) REFERENCES users(id)
+);
